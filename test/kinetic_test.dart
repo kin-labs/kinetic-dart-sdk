40eeb0dc67637ca69d3dfa3da485176ea75b3a0d
cryptobarbossa <cryptobarbossa@protonmail.com>
2022-09-20 12:08:42 +0100

    Edited tests

--- test/kinetic_test.dart
+++ test/kinetic_test.dart
@@ -21,96 +21,103 @@ KineticSdkConfig defaultConfig = KineticSdkConfig(
 
 void main() async {
   test('Get App Config', () async {
-    final kinetic = KineticSdk();
-    bool ok = await kinetic.setup(sdkConfig: defaultConfig);
-    if (ok) {
-      safePrint(kinetic.appConfig.toString());
+    await KineticSdk.setup(sdkConfig: defaultConfig);
+    bool ok = false;
+    if (KineticSdk().appConfig != null) {
+      ok = true;
+      safePrint(KineticSdk().appConfig.toString());
     }
     expect(ok, true);
   });
 
   test('Get Balance', () async {
-    final kinetic = KineticSdk();
+    await KineticSdk.setup(sdkConfig: defaultConfig);
+    bool ok = false;
 
-    bool ok = await kinetic.setup(sdkConfig: defaultConfig);
-    if (ok) {
-      GetBalanceOptions balanceOptions = GetBalanceOptions(account: Keypair().publicKeyFromString("DUXaDD5FZDa9yFf83tP8Abb6z66ECiawRShejSXRMN5F"));
-      dynamic res = await kinetic.getBalance(balanceOptions: balanceOptions);
-      safePrint(res);
+    GetBalanceOptions balanceOptions = GetBalanceOptions(account: Keypair().publicKeyFromString("DUXaDD5FZDa9yFf83tP8Abb6z66ECiawRShejSXRMN5F"));
+    dynamic res = await KineticSdk().getBalance(balanceOptions: balanceOptions);
+    if (res != null) {
+      ok = true;
     }
+    safePrint(res);
     expect(ok, true);
   });
 
   test('Get History', () async {
-    final kinetic = KineticSdk();
+    await KineticSdk.setup(sdkConfig: defaultConfig);
+    bool ok = false;
 
-    bool ok = await kinetic.setup(sdkConfig: defaultConfig);
-    if (ok) {
-      GetHistoryOptions historyOptions = GetHistoryOptions(account: Keypair().publicKeyFromString("DUXaDD5FZDa9yFf83tP8Abb6z66ECiawRShejSXRMN5F"), mint: Keypair().publicKeyFromString("KinDesK3dYWo3R2wDk6Ucaf31tvQCCSYyL8Fuqp33GX"));
-      dynamic res = await kinetic.getHistory(historyOptions: historyOptions);
-      safePrint(res);
+    GetHistoryOptions historyOptions = GetHistoryOptions(account: Keypair().publicKeyFromString("DUXaDD5FZDa9yFf83tP8Abb6z66ECiawRShejSXRMN5F"), mint: Keypair().publicKeyFromString("KinDesK3dYWo3R2wDk6Ucaf31tvQCCSYyL8Fuqp33GX"));
+    dynamic res = await KineticSdk().getHistory(historyOptions: historyOptions);
+    if (res != null) {
+      ok = true;
     }
+    safePrint(res);
+
     expect(ok, true);
   });
 
   test('Get Token Accounts', () async {
-    final kinetic = KineticSdk();
+    await KineticSdk.setup(sdkConfig: defaultConfig);
+    bool ok = false;
 
-    bool ok = await kinetic.setup(sdkConfig: defaultConfig);
-    if (ok) {
-      GetTokenAccountsOptions accountOptions = GetTokenAccountsOptions(account: Keypair().publicKeyFromString("DUXaDD5FZDa9yFf83tP8Abb6z66ECiawRShejSXRMN5F"), mint: Keypair().publicKeyFromString("KinDesK3dYWo3R2wDk6Ucaf31tvQCCSYyL8Fuqp33GX"));
-      dynamic res = await kinetic.getTokenAccounts(tokenAccountsOptions: accountOptions);
-      safePrint(res);
+    GetTokenAccountsOptions accountOptions = GetTokenAccountsOptions(account: Keypair().publicKeyFromString("DUXaDD5FZDa9yFf83tP8Abb6z66ECiawRShejSXRMN5F"), mint: Keypair().publicKeyFromString("KinDesK3dYWo3R2wDk6Ucaf31tvQCCSYyL8Fuqp33GX"));
+    dynamic res = await KineticSdk().getTokenAccounts(tokenAccountsOptions: accountOptions);
+    if (res != null) {
+      ok = true;
     }
+    safePrint(res);
+
     expect(ok, true);
   });
 
   test('Request Airdrop', () async {
-    final kinetic = KineticSdk();
+    await KineticSdk.setup(sdkConfig: defaultConfig);
+    bool ok = false;
 
-    bool ok = await kinetic.setup(sdkConfig: defaultConfig);
-    if (ok) {
-      RequestAirdropRequest airdropRequest = RequestAirdropRequest(account: "DUXaDD5FZDa9yFf83tP8Abb6z66ECiawRShejSXRMN5F", mint: "KinDesK3dYWo3R2wDk6Ucaf31tvQCCSYyL8Fuqp33GX", amount: "10", commitment: RequestAirdropRequestCommitmentEnum.finalized, index: kinetic.sdkConfig.index, environment: 'devnet');
-      dynamic res = await kinetic.requestAirdrop(airdropRequest: airdropRequest);
-      safePrint(res);
+    RequestAirdropRequest airdropRequest = RequestAirdropRequest(account: "DUXaDD5FZDa9yFf83tP8Abb6z66ECiawRShejSXRMN5F", mint: "KinDesK3dYWo3R2wDk6Ucaf31tvQCCSYyL8Fuqp33GX", amount: "10", commitment: RequestAirdropRequestCommitmentEnum.finalized, index: KineticSdk().sdkConfig.index, environment: 'devnet');
+    dynamic res = await KineticSdk().requestAirdrop(airdropRequest: airdropRequest);
+    if (res != null) {
+      ok = true;
     }
+    safePrint(res);
+
     expect(ok, true);
   });
 
   test('makeTransfer', () async {
-    final kinetic = KineticSdk();
+    await KineticSdk.setup(sdkConfig: defaultConfig);
+    bool ok = false;
 
-    bool ok = await kinetic.setup(sdkConfig: defaultConfig);
-    if (ok) {
-      final from = await Keypair().fromByteArray([230, 245, 154, 108, 75, 133, 168, 61, 98, 99, 138, 19, 150, 122, 171, 162, 56, 101, 142, 67, 229, 255, 240, 158, 249, 113, 155, 58, 155, 128, 209, 10]);
-      // final from = await Ed25519HDKeyPair.fromPrivateKeyBytes(
-      //   privateKey: [230, 245, 154, 108, 75, 133, 168, 61, 98, 99, 138, 19, 150, 122, 171, 162, 56, 101, 142, 67, 229, 255, 240, 158, 249, 113, 155, 58, 155, 128, 209, 10],
-      // );
+    final from = await Keypair().fromByteArray([230, 245, 154, 108, 75, 133, 168, 61, 98, 99, 138, 19, 150, 122, 171, 162, 56, 101, 142, 67, 229, 255, 240, 158, 249, 113, 155, 58, 155, 128, 209, 10]);
 
-      safePrint("From: ${from.publicKey.toBase58()}");
-      safePrint("To: AVGAggsdHmubCZLmJ94dRp98kGJu1ZsFENPTNSe3Nhfw");
+    safePrint("From: ${from.publicKey.toBase58()}");
+    safePrint("To: AVGAggsdHmubCZLmJ94dRp98kGJu1ZsFENPTNSe3Nhfw");
 
-      MakeTransferOptions makeTransferOptions = MakeTransferOptions(amount: "1.0", destination: Keypair().publicKeyFromString("AVGAggsdHmubCZLmJ94dRp98kGJu1ZsFENPTNSe3Nhfw"), commitment: MakeTransferRequestCommitmentEnum.finalized, mint: "KinDesK3dYWo3R2wDk6Ucaf31tvQCCSYyL8Fuqp33GX", owner: from, referenceId: "p2p", referenceType: "tx", type: TransactionType.p2p, senderCreate: true);
-      Transaction? transaction = await kinetic.makeTransfer(makeTransferOptions: makeTransferOptions);
-
-      safePrint(transaction?.toJson());
+    MakeTransferOptions makeTransferOptions = MakeTransferOptions(amount: "1.0", destination: Keypair().publicKeyFromString("AVGAggsdHmubCZLmJ94dRp98kGJu1ZsFENPTNSe3Nhfw"), commitment: MakeTransferRequestCommitmentEnum.finalized, mint: "KinDesK3dYWo3R2wDk6Ucaf31tvQCCSYyL8Fuqp33GX", owner: from, referenceId: "p2p", referenceType: "tx", type: TransactionType.p2p, senderCreate: true);
+    Transaction? transaction = await KineticSdk().makeTransfer(makeTransferOptions: makeTransferOptions);
+    if (transaction != null) {
+      ok = true;
     }
+    safePrint(transaction?.toJson());
+
     expect(ok, true);
   }, timeout: const Timeout(Duration(minutes: 10)), );
 
   test('createAccount', () async {
-    final kinetic = KineticSdk();
+    await KineticSdk.setup(sdkConfig: defaultConfig);
+    bool ok = false;
 
-    bool ok = await kinetic.setup(sdkConfig: defaultConfig);
-    if (ok) {
-      Keypair keypair = Keypair();
-      final from = await keypair.random();
+    Keypair keypair = Keypair();
+    final from = await keypair.random();
 
-      CreateAccountOptions createAccountOptions = CreateAccountOptions(owner: from, mint: "KinDesK3dYWo3R2wDk6Ucaf31tvQCCSYyL8Fuqp33GX", commitment: CreateAccountRequestCommitmentEnum.finalized, referenceId: 'dart', referenceType: 'test');
-      Transaction? transaction = await kinetic.createAccount(createAccountOptions: createAccountOptions);
-
-      safePrint(transaction?.toJson());
+    CreateAccountOptions createAccountOptions = CreateAccountOptions(owner: from, mint: "KinDesK3dYWo3R2wDk6Ucaf31tvQCCSYyL8Fuqp33GX", commitment: CreateAccountRequestCommitmentEnum.finalized, referenceId: 'dart', referenceType: 'test');
+    Transaction? transaction = await KineticSdk().createAccount(createAccountOptions: createAccountOptions);
+    if (transaction != null) {
+      ok = true;
     }
+    safePrint(transaction?.toJson());
+
     expect(ok, true);
   }, timeout: const Timeout(Duration(minutes: 10)), );
 
