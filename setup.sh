e573a4f8f0f1a53036b8035d7d2421ed74b99b64
Bram Borggreve <beeman@beeman.nl>
2022-09-22 17:00:58 +0100

    chore: clean up generated output

--- setup.sh
+++ setup.sh
@@ -1,1 +1,3 @@
-rm -rf lib/generated/ ; npx openapi-generator-cli generate -i https://raw.githubusercontent.com/kin-labs/kinetic/dev/api-swagger.json -g dart -o lib/generated/ ; rm -rf lib/generated/test
+rm -rf lib/generated/ \
+  && npx openapi-generator-cli generate -i https://raw.githubusercontent.com/kin-labs/kinetic/dev/api-swagger.json -g dart -o lib/generated/ \
+  && rm -rf lib/generated/{.openapi-generator,doc,test,git_push.sh,README.md,analysis_options.yaml,.travis.yml,pubspec.yaml,.openapi-generator-ignore,.gitignore}
