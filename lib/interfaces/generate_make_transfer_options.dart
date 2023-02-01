0e809f6d82888427bf2bce9d7d7f013859db6446
beeman ⬢ <beeman@beeman.nl>
2023-02-01 16:21:46 +0000

    refactor: use getAccountInfo to fetch token accounts on makeTransfer (#20)

--- lib/interfaces/generate_make_transfer_options.dart
+++ lib/interfaces/generate_make_transfer_options.dart
@@ -7,12 +7,14 @@ class GenerateMakeTransferOptions {
     required this.amount,
     required this.blockhash,
     required this.destination,
+    required this.destinationTokenAccount,
     required this.index,
     required this.lastValidBlockHeight,
     required this.mintDecimals,
     required this.mintFeePayer,
     required this.mintPublicKey,
     required this.owner,
+    required this.ownerTokenAccount,
     required this.type,
     this.senderCreate,
   });
@@ -21,12 +23,14 @@ class GenerateMakeTransferOptions {
   late String amount;
   late String blockhash;
   late String destination;
+  late String destinationTokenAccount;
   late int index;
   late int lastValidBlockHeight;
   late int mintDecimals;
   late String mintFeePayer;
   late String mintPublicKey;
   late Keypair owner;
+  late String ownerTokenAccount;
   late bool? senderCreate;
   late TransactionType type;
 }
